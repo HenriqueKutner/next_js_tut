802ea719922cfc10fbdea39c7784bbccb2b61281
Henrique Kutner Cordeiro <kutner.cordeiro@gmail.com>
2021-04-22 20:41:48 -0300

    'Adding'

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <h1>HomePage</h1>
+      <Footer />
     </div>
   )
 }
