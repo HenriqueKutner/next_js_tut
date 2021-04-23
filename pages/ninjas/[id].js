4b6d003692b8911d4d589787db2a3f9a874bf0ab
Henrique Kutner Cordeiro <kutner.cordeiro@gmail.com>
2021-04-23 15:22:41 -0300

    single item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,16 +1,37 @@
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+
     return {
-        paths: {ninjas: data}
+        paths: paths, // And the it will know how many html pages to create based on this
+        fallback: false
+    }
+}
+
+export const getStaticProps = async () => {
+    const id = context.params.id;
+    const res = fetch('https://jsonplaceholder.typicode.com/users' + id);
+    const data = await res.json();
+
+    return {
+        props: { ninja: data }
     }
 }
 
 
-const Details = () => {
+const Details = ({ ninja }) => {
     return ( 
         <div>
-            <h1>Details Page</h1>
+            <h1>{ ninja.name }</h1>
+            <h1>{ ninja.email }</h1>
+            <h1>{ ninja.website }</h1>
+            <h1>{ ninja.adress.city }</h1>
         </div>
      );
 }
