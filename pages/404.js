50295efaa2ad1d1eac39a98c195ad5724d0700bb
Henrique Kutner Cordeiro <kutner.cordeiro@gmail.com>
2021-04-23 13:42:25 -0300

    'redirecting-users'

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from ' next/router'
 import Link from 'next/link'
 
 const NotFound = () => {
+
+    const router = useRouter()
+    console.log(router)
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+            console.log(5)
+        }, 3000)
+    },[])
+
     return (
         <div className="not-found">
             <h1>Oooooops...</h1>
