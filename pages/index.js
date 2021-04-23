928d823025d9d6cd69e8e268be2f69c85e8f2fcd
Henrique Kutner Cordeiro <kutner.cordeiro@gmail.com>
2021-04-22 21:09:40 -0300

    'le'

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
       <h1>HomePage</h1>
+      <p>Lorem</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
